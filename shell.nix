af40099553b9af12e137d39b92253c83d87c0f9c
James Brock <jamesbrock@gmail.com>
2022-01-30 15:58:46 +0900

    Upgrade

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
 
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchGit {
+  # https://github.com/NixOS/nixpkgs/releases/tag/21.05
+  url = "https://github.com/nixos/nixpkgs/";
+  ref = "refs/tags/21.05";
+  rev = "7e9b0dff974c89e070da1ad85713ff3c20b0ca97";
+  }) {}
+}:
+
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     nodejs-14_x
